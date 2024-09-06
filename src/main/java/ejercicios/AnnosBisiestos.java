76e65bdeb721c608e0b5873e4f7bce53ee21c1ce
Lina velasco <linam.velascod@konradlorenz.edu.co>
2024-09-06 10:46:38 -0500

    años bisiestos
    calcular si un año es bisiesto

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -10,7 +10,11 @@ import java.util.Scanner;
  */
 public class AnnosBisiestos {
     public static String evaluar(int anno){
-        // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
+        if (anno % 4 ==0 && anno % 100 !=0){
+            System.out.println(anno + " es bisiesto");
+        }else {System.out.println(anno + " no es bisiesto");
+        
+        }
         return "";
     }
     
